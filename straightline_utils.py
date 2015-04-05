2b103f1156a1dccfd7ea3dacc5897ea0185c1538
Dustin Lang <dstndstn@gmail.com>
2015-04-04 18:05:34 -0700

    mayacamas edition of straightline notebook

--- straightline_utils.py
+++ straightline_utils.py
@@ -11,7 +11,7 @@ import pylab as plt
 data1 = np.array([[201,592,61],[244,401,25],[47,583,38],[287,402,15],[203,495,21],
                   [58,173,15],[210,479,27],[202,504,14],[198,510,30],[158,416,16],
                   [165,393,14],[201,442,25],[157,317,52],[131,311,16],[166,400,34],
-                  [160,337,31],[186,423,42],[125,334,26],[218,533,16],[146,344,22]])
+                  [160,337,31],[186,423,42],[125,334,26],[218,533,16],[146,344,22]]).astype(float)
 
 # plotting limits
 xlimits = [0,250]
@@ -19,8 +19,51 @@ ylimits = [100,600]
 title_prefix = 'Straight line'
 plot_format = '.png'
 
-mlimits = [1.5, 3.5]
-blimits = [0, 60]
+mlimits = [1.9, 2.6]
+blimits = [-20, 80]
+mlo,mhi = mlimits
+blo,bhi = blimits
+slo,shi = [0.001,100]
+
+def pdf_contour_levels(p):
+    sortp = np.sort(p.ravel())
+    cump = sortp.cumsum()
+    return [sortp[cump > cump.max() * f].min()
+            for f in [0.32, 0.05]]
+
+def plot_mcmc_results(chain):
+    # Pull m and b arrays out of the Markov chain.
+    mm = [m for b,m in chain]
+    bb = [b for b,m in chain]
+    # Scatterplot of m,b posterior samples
+    plt.clf()
+    plt.contour(bgrid, mgrid, posterior, pdf_contour_levels(posterior))
+    plt.plot(bb, mm, 'b.', alpha=0.1)
+    plot_mb_setup()
+    plt.show()
+    
+    # Histograms
+    import triangle
+    triangle.corner(chain, labels=['b','m'], extents=[0.99]*2)
+    plt.show()
+    
+    # Traces
+    plt.clf()
+    plt.subplot(2,1,1)
+    plt.plot(mm, 'k-')
+    plt.ylim(mlo,mhi)
+    plt.ylabel('m')
+    plt.subplot(2,1,2)
+    plt.plot(bb, 'k-')
+    plt.ylabel('b')
+    plt.ylim(blo,bhi)
+    plt.show()
+
+
+def plot_mb_setup():
+    plt.xlabel('intercept b')
+    plt.ylabel('slope m')
+    plt.axis([blo,bhi, mlo,mhi])
 
 def get_data_no_outliers():
     # pull out the x, y, and sigma_y columns, which have been packed into the
@@ -31,6 +74,12 @@ def get_data_no_outliers():
     y = data1[5:,1]
     sigmay = data1[5:,2]
     return (x, y, sigmay)
+
+def get_data_with_outliers():
+    x = data1[:,0]
+    y = data1[:,1]
+    sigmay = data1[:,2]
+    return x,y,sigmay
 
 # Plot data with error bars, standard axis limits, etc.
 def plot_yerr(x, y, sigmay):
